ba3e4a1e43a15e3b7986dfd1066dc2c7f920e413
cyctw <yuaannn_192.cs06@g2.nctu.edu.tw>
2022-04-19 20:23:02 +0800

    fix dotenv setting

--- server.go
+++ server.go
@@ -5,21 +5,18 @@ import (
 	"os"
 
 	"github.com/gin-gonic/gin"
-	"github.com/joho/godotenv"
 	"github.com/line/line-bot-sdk-go/v7/linebot"
 )
 
 func main() {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
 
 	r := gin.Default()
 	channel_secret := os.Getenv("CHANNEL_SECRET")
 	channel_access_token := os.Getenv("CHANNEL_ACCESS_TOKEN")
 	bot, err := linebot.New(channel_secret, channel_access_token)
-
+	if err != nil {
+		log.Print("Line bot initiate failed")
+	}
 	r.POST("/callback", func(c *gin.Context) {
 		events, err := bot.ParseRequest(c.Request)
 		if err != nil {
